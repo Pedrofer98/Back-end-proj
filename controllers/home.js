1a1c4aa4b89c11f84ea6e25af92aa59c4faf3258
Jesse Oudom <jesse.oudom@yahoo.com>
2022-10-01 13:43:22 -0400

    updates fetch on home.js

--- controllers/home.js
+++ controllers/home.js
@@ -1,8 +1,19 @@
-const leagueStats = {
-    method: 'GET',
-  };
-  
-  fetch("https://soccer.sportmonks.com/api/v2.0/standings/season/19735?api_token=ljGMMrxuRdt4Uq0FHzS6k9KhmsOjCE5EwwdMMPZ4xQYW96KIzmPAtIY6Vnid", leagueStats)
-    .then(response => response.text())
-    .then(result => console.log(result))
-    .catch(error => console.log('error', error));
+const leagueAPI = 'https://soccer.sportmonks.com/api/v2.0/standings/season/19735?api_token=ljGMMrxuRdt4Uq0FHzS6k9KhmsOjCE5EwwdMMPZ4xQYW96KIzmPAtIY6Vnid';
+
+fetch(leagueAPI)
+  .then(response => response.json())
+  .then(json => {
+    console.log(json.data[0].standings.data);
+  });
+
+// let html = "<table>";
+// for (let i = 0; i < statsArray.length; i++) {
+//     html+="<tr>";
+//     html+="<td>"+statsArray[i].position+"</td>";
+//     html+="<td>"+statsArray[i].team_name+"</td>";
+//     html+="<td>"+statsArray[i].overall.points+"</td>";
+//     html+="</tr>";
+// }
+
+// html+="</table>";
+// document.getElementById("table").innerHTML = html;
