12e436776f91fa073b978208743c889cf3ce0354
Jesse Oudom <jesse.oudom@yahoo.com>
2022-10-03 05:01:03 -0400

    updates fetch and stats functions

--- controllers/home.js
+++ controllers/home.js
@@ -1,19 +1,33 @@
-const leagueAPI = 'https://soccer.sportmonks.com/api/v2.0/standings/season/19735?api_token=ljGMMrxuRdt4Uq0FHzS6k9KhmsOjCE5EwwdMMPZ4xQYW96KIzmPAtIY6Vnid';
+// Puts API in a variable
+const leagueStatsAPI = 'https://soccer.sportmonks.com/api/v2.0/standings/season/19735?api_token=ljGMMrxuRdt4Uq0FHzS6k9KhmsOjCE5EwwdMMPZ4xQYW96KIzmPAtIY6Vnid';
 
-fetch(leagueAPI)
-  .then(response => response.json())
-  .then(json => {
-    console.log(json.data[0].standings.data);
-  });
+// Calls API and turns it into json
+fetch(leagueStatsAPI)
+.then(response => response.json())
+.then(json => {
+  // Dives into json and creates a new array with only the info we need
+  const data = json.data[0].standings.data;
+  const statsArray = new Array();
+    for (let i = 0; i < data.length; i++) {
+      statsArray.push(stats(data[i]));
+    }
+    // Points the new array towards the html
+    Promise.all(statsArray).then(html => {
+      document.querySelector('#stats').innerHTML = html.join('');
+    });
+});
 
-// let html = "<table>";
-// for (let i = 0; i < statsArray.length; i++) {
-//     html+="<tr>";
-//     html+="<td>"+statsArray[i].position+"</td>";
-//     html+="<td>"+statsArray[i].team_name+"</td>";
-//     html+="<td>"+statsArray[i].overall.points+"</td>";
-//     html+="</tr>";
-// }
-
-// html+="</table>";
-// document.getElementById("table").innerHTML = html;
+// Enters the new array into the html
+async function stats(data) {
+  console.log(data);
+  return  `
+            <tr>
+              <td>${data.position}</td>
+              <td>${data.team_name}</td>
+              <td>${data.points}</td>
+              <td>${data.overall.won}</td>
+              <td>${data.overall.draw}</td>
+              <td>${data.overall.lost}</td>
+            </tr>
+          `
+};
