4eb4712a175dcbca3b8d569d6951e9ff8cfbb438
Your Name <pedro.fer.98@gmail.com>
2022-10-03 22:30:23 -0400

    sql files

---
+++ databases/seed.sql
@@ -0,0 +1,17 @@
+create table playerInfo (
+    {data.fullname} varchar,
+
+    {data.birthcountry} varchar,
+
+    {data.nationality} varchar,
+
+    {data.birthdate} varchar,
+
+    {data.height} varchar,
+
+    {data.team_id} varchar,
+
+)
+
+
+       
