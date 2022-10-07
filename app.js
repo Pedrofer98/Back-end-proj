ad02483f8022696619dbbb97defbc44c73fbdb91
Jesse Oudom <jesse.oudom@yahoo.com>
2022-10-07 00:25:42 -0400

    adds new favored& unfavored routes to express app

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 // Imports
 const express = require('express');
-const homeRoutes = require('./routes/homeRoutes')
-const profileRoutes = require('./routes/profileRoutes')
-const playersRoutes = require('./routes/playersRoutes')
+const homeRoutes = require('./routes/homeRoutes');
+const profileRoutes = require('./routes/profileRoutes');
+const playersRoutes = require('./routes/playersRoutes');
+
+const favTeamRoutes = require('./routes/favTeamRoutes');
+const unfavTeamRoutes = require('./routes/unfavTeamRoutes');
+
+const favPlayerRoutes = require('./routes/favPlayerRoutes.js');
+const unfavPlayerRoutes = require('./routes/unfavPlayerRoutes');
+
 const app = express();
 
 // Server
@@ -13,7 +20,12 @@ app.use(homeRoutes);
 app.use(profileRoutes);
 app.use(playersRoutes);
 
+app.use(favTeamRoutes);
+app.use(unfavTeamRoutes);
+app.use(favPlayerRoutes);
+app.use(unfavPlayerRoutes);
+
 // Middleware
 app.use(express.static(__dirname + '/public'));
-app.use('/controllers', express.static('controllers'))
+app.use('/controllers', express.static('controllers'));
 app.set('view engine', 'ejs');
