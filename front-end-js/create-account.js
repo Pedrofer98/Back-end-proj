ac31f8bdf2d58fd9ce190b3123a3ec8bbedf29a3
aronCampos <aron.campos400@gmail.com>
2022-10-08 12:05:31 -0700

    update

--- front-end-js/create-account.js
+++ front-end-js/create-account.js
@@ -1,11 +1,25 @@
+const addRowFunction = require('../models/user')
+
 window.onload=function() {
     const submitButton = document.getElementById('create-btn');
     submitButton.addEventListener('click', createAccount);
+    submitButton.addEventListener('click', addRowFunction.addRow());
+    console.log(addRowFunction.addRow())
 }
+
+const username = ''
+const password = ''
 
 function createAccount(){
     const username = document.getElementById('username').value
     const password = document.getElementById('password').value
-    console.log('test')
+    console.log('username')
     
 }
+
+module.exports = {
+    username,
+    password
+
+}
+
