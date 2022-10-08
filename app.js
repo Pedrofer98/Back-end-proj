f6d6a72689a999ab34df2514625d829859bb630f
aronCampos <aron.campos400@gmail.com>
2022-10-08 11:30:15 -0700

    Fix CMV

--- app.js
+++ app.js
@@ -1,32 +1,15 @@
 // Imports
 const express = require('express');
-const homeRoutes = require('./routes/homeRoutes');
-const profileRoutes = require('./routes/profileRoutes');
-const playersRoutes = require('./routes/playersRoutes');
 
-const createAccount = require('./routes/create-account');
-
-const favTeamRoutes = require('./routes/favTeamRoutes');
-const unfavTeamRoutes = require('./routes/unfavTeamRoutes');
-
-const favPlayerRoutes = require('./routes/favPlayerRoutes.js');
-const unfavPlayerRoutes = require('./routes/unfavPlayerRoutes');
-
+const routes = require('./routes/routes');
 const app = express();
 
 // Server
 app.listen(3000);
 
 // Routes
-app.use(homeRoutes);
-app.use(profileRoutes);
-app.use(playersRoutes);
-app.use(createAccount);
+app.use(routes);
 
-app.use(favTeamRoutes);
-app.use(unfavTeamRoutes);
-app.use(favPlayerRoutes);
-app.use(unfavPlayerRoutes);
 
 // Middleware
 app.use(express.static(__dirname + '/public'));
